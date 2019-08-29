9de5128b98eee4d7fac52b46376a62cfeca4a5dc
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-08-29 20:48:08 +0200

    Use click

--- munkresJu.py
+++ munkresJu.py
@@ -3,6 +3,7 @@ from munkres import Munkres # Hungarian algorithm
 import csv # opening and reading csv files
 from sklearn.impute import SimpleImputer
 import json
+import click
 
 
 """
@@ -12,7 +13,7 @@ The csv files are to be placed in ./data.
 
 def compatPercentage(n):
     if n >= 500:
-        n -= 500
+        n -= 500  # Remove the cost caused by cloning
     return '{:2.2f}%'.format(100 * max(0, 1 - n / 25))
 
 def n_inv(r1, r2):
@@ -54,7 +55,8 @@ def shareFavLangCost(criterion, frRow, exRow):
                 set(exRow[learningQ].split(','))]
     fav = {'fr': favT[frRow[favQ]],
            'ex': favT[exRow[favQ]]}
-    return int(len(frLangs[fav['fr']].intersection(exLangs[fav['ex']])) == 0)  # Do they have no preferred language in common ?
+    # Do they have no preferred language in common ?
+    return int(len(frLangs[fav['fr']].intersection(exLangs[fav['ex']])) == 0)
 
 def semiFavLangCost(criterion, frRow, exRow):
     """Returns 0 if the two share a language that is a favorite for one of the two, else 1"""
@@ -69,7 +71,8 @@ def semiFavLangCost(criterion, frRow, exRow):
     fav = {'fr': favT[frRow[favQ]],
            'ex': favT[exRow[favQ]]}
     # Do they have no language that both of them speak, but one would prefer not to speak ?        
-    return int(len(frLangs[1 - fav['fr']].intersection(exLangs[fav['ex']]).union(frLangs[1 - fav['fr']].intersection(exLangs[1 - fav['ex']]))) == 0)
+    return int(len(frLangs[1 - fav['fr']].intersection(exLangs[fav['ex']])\
+            .union(frLangs[fav['fr']].intersection(exLangs[1 - fav['ex']]))) == 0)
 
 def sharedLangCost(criterion, frRow, exRow):
     """Returns 1 if the two do not share a language, else 0"""
@@ -80,7 +83,8 @@ def sharedLangCost(criterion, frRow, exRow):
     exLangs = [set(exRow[fluentQ].split(',')),
                 set(exRow[learningQ].split(','))]
     # Do they share no language ?
-    return int(len(frLangs[0].union(frLangs[1]).intersection(exLangs[0].union(exLangs[1]))) == 0)
+    return int(len(frLangs[0].union(frLangs[1])\
+     .intersection(exLangs[0].union(exLangs[1]))) == 0)
 
 def hasIntersectionCost(criterion, frRow, exRow):
     """Returns 1 if the two do not share an answer on the specified question, else 0"""
@@ -129,11 +133,13 @@ costFuncs = {
 }
 
 def andCond(cond, frRow, exRow):
+    """Parses an and condition and checks whether it is true on these rows"""
     c2 = cond["condition2"]
     c1 = cond["condition1"]
     return resolveCond(c1, frRow, exRow) and resolveCond(c2, frRow, exRow)
 
 def equalsCond(cond, frRow, exRow):
+    """Parses an equals condition and checks whether it is true on these rows"""
     q = cond["QLabel"]
     v = cond["value"]
     w = cond["who"]
@@ -151,6 +157,7 @@ condFuncs = {
 }
 
 def resolveCond(cond, frRow, exRow):
+    """Parses a condition and checks whether it is true on these rows"""
     return condFuncs[cond["type"]](cond, frRow, exRow)
 
 def totalCost(criterias, frRow, exRow):
@@ -161,10 +168,10 @@ def totalCost(criterias, frRow, exRow):
     return cost
 
 def prepare(answers, doClone=False, cloneLabel=None):
+    """Prepare the answers dataframe"""
     # Drop title indexes
     answers = answers.drop(index=[0, 1])
     answers = answers.reset_index(drop=True)
-
     # Ad-hoc solution so that the imputer does not drop the empty columns
     problemColumns = ["RecipientLastName",
         "RecipientFirstName",
@@ -176,7 +183,6 @@ def prepare(answers, doClone=False, cloneLabel=None):
     imp = SimpleImputer(strategy="most_frequent")
     tmp = imp.fit_transform(answers)
     answers = pd.DataFrame(tmp, columns=answers.columns)
-    
     # Clone the frenchies who accept to take two exchange partners
     if doClone:
         clones = answers.copy(deep=True)[answers[cloneLabel].map(repeat.get)]
@@ -190,62 +196,97 @@ def prepare(answers, doClone=False, cloneLabel=None):
     return answers
 
 
-with open('config.json', 'r') as conf_file:
+@click.command()
+@click.option("--conf-file", '-c',
+              default="config.json",
+              type=click.File(),
+              help="The JSON configuration file")
+@click.option("--french-ans", '-f',
+              default="./data/french.csv",
+              type=click.Path(exists=True, dir_okay=False),
+              help="The french answers CSV file")
+@click.option("--exchange-ans", '-e',
+              default="./data/exchange.csv",
+              type=click.Path(exists=True, dir_okay=False),
+              help="The exchange student answers CSV file")
+@click.option("--remaining-fr", '-rf',
+              default="./output/remainingFr.csv",
+              type=click.File(mode="w"),
+              help="The file into which the remaining unmatched french should be outputted")
+@click.option("--remaining-ex", '-re',
+              default="./output/remainingEx.csv",
+              type=click.File(mode="w"),
+              help="The file into which the remaining unmatched exchange students should be outputted")
+@click.option("--matchings-file", '-m',
+              default="./output/matchings.csv",
+              type=click.File(mode="w"),
+              help="The file into which the remaining unmatched french should be outputted")
+@click.option("--verbose", '-v',
+              count=True,
+              help="Show more info in stderror")
+def match(conf_file, french_ans, exchange_ans, remaining_fr, remaining_ex, matchings_file, verbose):
     config = json.load(conf_file)
-criterias = config['criterias']
+    criterias = config['criterias']
 
-# import data
-fr = pd.read_csv("./data/french.csv")
-ex = pd.read_csv("./data/exchange.csv")
+    # import data
+    fr = pd.read_csv(french_ans)
+    ex = pd.read_csv(exchange_ans)
 
-frAnswers = prepare(fr, doClone=True, cloneLabel=config['two_buddies_question_label'])
-exAnswers = prepare(ex)
+    frAnswers = prepare(fr, doClone=True, cloneLabel=config['two_buddies_question_label'])
+    exAnswers = prepare(ex)
 
-# Compute the costs 
-costMatrix = [[float('inf')] * len(exAnswers.index) for _ in range(len(frAnswers.index))]
-for fridx, fr_row in frAnswers.iterrows():
-    for exidx, ex_row in exAnswers.iterrows():
-        # Compute a list of compatibility scores
-        # Scores are normalized between 0 and 1
-        costMatrix[fridx][exidx] = totalCost(criterias, fr_row, ex_row)
+    # Compute the costs 
+    costMatrix = [[float('inf')] * len(exAnswers.index) for _ in range(len(frAnswers.index))]
+    for fridx, fr_row in frAnswers.iterrows():
+        for exidx, ex_row in exAnswers.iterrows():
+            # Compute a list of compatibility scores
+            # Scores are normalized between 0 and 1
+            costMatrix[fridx][exidx] = totalCost(criterias, fr_row, ex_row)
 
-# Solve the assignment problem using munkres
-m = Munkres()
-assignments = m.compute(costMatrix)
+    # Solve the assignment problem using munkres
+    m = Munkres()
+    assignments = m.compute(costMatrix)
 
-# Print results
-for fridx, exidx in assignments:
-    print(frAnswers['Q1'][fridx], frAnswers['Q2'][fridx],
-          '<3',
-          exAnswers['Q1'][exidx], exAnswers['Q2'][exidx],
-          compatPercentage(costMatrix[fridx][exidx]))
+    # Print results
+    if verbose > 0:
+        for fridx, exidx in assignments:
+            print(frAnswers['Q1'][fridx], frAnswers['Q2'][fridx],
+                '<3',
+                exAnswers['Q1'][exidx], exAnswers['Q2'][exidx],
+                compatPercentage(costMatrix[fridx][exidx]))
 
 
-# Output remaining unmatched people
-frMatched, exMatched = zip(*assignments)
-frAnswers[~frAnswers.index.isin(frMatched)].to_csv("./output/remainingFr.csv")
-exAnswers[~exAnswers.index.isin(exMatched)].to_csv("./output/remainingEx.csv")
+    # Output remaining unmatched people
+    frMatched, exMatched = zip(*assignments)
+    frAnswers[~frAnswers.index.isin(frMatched)].to_csv(remaining_fr)
+    exAnswers[~exAnswers.index.isin(exMatched)].to_csv(remaining_ex)
 
-# Outputing matchings
-matchings = pd.DataFrame(columns=["frFName", "frLName", "frEMail", "exFName", "exLName", "exEMail", "language"])
-for fridx, exidx in assignments:
-    fQ, lQ = config["fluentQ"], config["learningQ"]
-    fnQ, lnQ, emQ = config["firstNameQ"], config["lastNameQ"], config["eMailQ"]
-    frRow = frAnswers.iloc[fridx]
-    exRow = exAnswers.iloc[exidx]
-    commonLangs = set(frRow[fQ].split(',')).intersection(set(exRow[fQ].split(',')))
-    for lang in config["mails"]:
-        if lang in commonLangs:
-            chosenLang = lang
-    else:
-        chosenLang = "English"
-    d = {"frFName": frRow[fnQ],
-         "frLName": frRow[lnQ],
-         "frEMail": frRow[emQ],
-         "exFName": exRow[fnQ],
-         "exLName": exRow[lnQ],
-         "exEMail": exRow[emQ],
-         "language": chosenLang}
-    matchings = matchings.append(d, ignore_index=True)
+    # Outputing matchings
+    matchings = pd.DataFrame(columns=["frFName",
+                                      "frLName",
+                                      "frEMail",
+                                      "exFName",
+                                      "exLName",
+                                      "exEMail",
+                                      "language"])
+    for fridx, exidx in assignments:
+        fQ, lQ = config["fluentQ"], config["learningQ"]
+        fnQ, lnQ, emQ = config["firstNameQ"], config["lastNameQ"], config["eMailQ"]
+        frRow = frAnswers.iloc[fridx]
+        exRow = exAnswers.iloc[exidx]
+        commonLangs = set(frRow[fQ].split(',')).intersection(set(exRow[fQ].split(',')))
+        for lang in config["mails"]:
+            if lang in commonLangs:
+                chosenLang = lang
+        else:
+            chosenLang = "English"
+        d = {"frFName": frRow[fnQ],
+            "frLName": frRow[lnQ],
+            "frEMail": frRow[emQ],
+            "exFName": exRow[fnQ],
+            "exLName": exRow[lnQ],
+            "exEMail": exRow[emQ],
+            "language": chosenLang}
+        matchings = matchings.append(d, ignore_index=True)
 
-matchings.to_csv("./output/matchings.csv")
+    matchings.to_csv("./output/matchings.csv")
